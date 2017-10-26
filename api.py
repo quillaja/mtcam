0d2d741d3b4a36eba1bd58195886d54dbb854b86
ben <quillaja@hotmail.com>
2017-10-26 03:36:58 -0700

    added a bunch of crap to make the input times to scrapes() be viewed as relative to the mountain (aka 'local time') or relative to the server ('sys time').

--- api.py
+++ api.py
@@ -1,11 +1,42 @@
 import json
 import datetime as dt
+import time
+from distutils.util import strtobool
 from flask import Flask, request
 import queries
 from model import Mountain, Cam, ScrapeRecord
 import settings
 
 app = Flask(__name__)
+
+
+def to_sys_time(t: dt.datetime, mttz: dict) -> dt.datetime:
+    '''Uses mttz and system tz info to recalculate `t` into system time.'''
+    # print(mttz, time.timezone, time.daylight)
+    if 'rawOffset' not in mttz or 'dstOffset' not in mttz:
+        return t
+
+    return (t - dt.timedelta(
+        seconds=(mttz['rawOffset'] + mttz['dstOffset']))) - (dt.timedelta(
+            seconds=time.timezone) - dt.timedelta(hours=time.daylight))
+
+
+def convert_input(time_input: str, as_local_time: bool=False,
+                  mttz: dict=None) -> dt.datetime:
+    '''
+    Parses a time string. If `as_local_time` is True, it will use the
+    timezone info provided in `mttz` and the system's timezone info to
+    convert `time_input` from the local time (at the mountain) to the
+    system's time.
+    '''
+    try:
+        time_input = dt.datetime.strptime(time_input, '%Y-%m-%dT%H:%M')
+        if as_local_time and (mttz is not None):
+            time_input = to_sys_time(time_input, mttz)
+    except ValueError:
+        time_input = None
+
+    return time_input
 
 
 @app.route('/api/data')
@@ -46,26 +77,27 @@ def data():
 
 @app.route('/api/mountains/<int:mt_id>/cams/<int:cam_id>/scrapes')
 def scrapes(mt_id, cam_id):
+    mt = Mountain.get(Mountain.id == mt_id)
+    cam = Cam.get(Cam.id == cam_id)
+
     end = request.args.get('end', '')
     start = request.args.get('start', '')
+    as_local_time = strtobool(request.args.get('as_local_time', 'false'))
 
     try:
-        end = dt.datetime.strptime(end, '%Y-%m-%dT%H:%M')
+        mttz = json.loads(mt.tz_json)
     except ValueError:
-        # will resolve to now() in the query func
-        end = None
+        # mt.tz_json was invalid json (including '')
+        mttz = None
 
-    try:
-        start = dt.datetime.strptime(start, '%Y-%m-%dT%H:%M')
-    except ValueError:
-        # these will be resolved to 'end'-24h in the query func
-        start = None
-
-    mt_path = Mountain.get(Mountain.id == mt_id).as_pathname()
-    cam_path = Cam.get(Cam.id == cam_id).as_pathname()
+    print(end, start, as_local_time, mttz)
+    end = convert_input(end, as_local_time, mttz)
+    start = convert_input(start, as_local_time, mttz)
 
     r = queries.scraperecords_for_cam(cam_id, start, end)
     data = list()
+    mt_path = mt.as_pathname()
+    cam_path = cam.as_pathname()
 
     for s in r:
         if s.result == ScrapeRecord.SUCCESS:
@@ -83,3 +115,9 @@ def scrapes(mt_id, cam_id):
         data.append(sd)
 
     return json.dumps(data, indent=2, sort_keys=True)
+
+
+# don't really need this
+# @app.errorhandler(404)
+# def error_stuff(error):
+#     return 'Error: {}'.format(str(error)), error.code
