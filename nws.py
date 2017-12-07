3f3fc5f20354a652874042c8c7f9d71be9b047e7
ben stabley <quillaja@hotmail.com>
2017-12-07 09:45:41 -0800

    mostly added rain/snow plots, with some other minor tweaks

--- nws.py
+++ nws.py
@@ -1,26 +1,16 @@
 import sys
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import requests
 from bs4 import BeautifulSoup
 from bokeh.plotting import figure, output_file, show
 
-# URL = "https://graphical.weather.gov/xml/sample_products/browser_interface/ndfdBrowserClientByDay.php?lat={}&lon={}&format=24+hourly&numDays=1&startDate={}"
-
-# params = {'lat': '45.373439', 'lon': '-121.695962', 'date': '2017-12-05'}
-# req_url = URL.format(params['lat'], params['lon'], params['date'])
-# responseSummarized = requests.get(req_url)
-
-# parsedSummarized = BeautifulSoup(responseSummarized.content, 'xml')
-
-# max temp
-# parsed.data.parameters.find_all('temperature')[0].value.text
-
 
 def scrape():
     '''get xml from NWS'''
     params = {
+        # Mt Hood, approximate in Devil's Kitchen
         'lat': '45.373439',
         'lon': '-121.695962',
         'product': 'time-series',
@@ -43,9 +33,9 @@ def scrape():
     }
 
     # TODO: make more robust, timeouts, etc
-    URL_NON_SUMMARIZED = "https://graphical.weather.gov/xml/sample_products/browser_interface/ndfdXMLclient.php"
-    responseNonsummarized = requests.get(URL_NON_SUMMARIZED, params=params)
-    pns = BeautifulSoup(responseNonsummarized.content, 'xml')
+    url_non_summarized = "https://graphical.weather.gov/xml/sample_products/browser_interface/ndfdXMLclient.php"
+    response = requests.get(url_non_summarized, params=params)
+    pns = BeautifulSoup(response.content, 'xml')
     return pns
 
 
@@ -62,11 +52,11 @@ def clean(soup_obj):
     wind_spd = soup_obj.find_all('wind-speed')[0].value.text
     wind_gust = soup_obj.find_all('wind-speed')[1].value.text
     wind_dir = soup_obj.find_all('direction')[0].value.text
-    # weather = soup_obj.find_all('weather')[0].weather_conditions
 
     retrieved = datetime.now()
     retrieved = retrieved.replace(microsecond=0)
 
+    # TODO: convert to numbers instead of leaving as strings
     data = {
         'retrieved': retrieved.isoformat(),
         'temp_max': temp_max,
@@ -77,7 +67,6 @@ def clean(soup_obj):
         'wind_spd': wind_spd,
         'wind_gust': wind_gust,
         'wind_dir': wind_dir,
-        # 'weather': weather
     }
 
     return data
@@ -85,7 +74,7 @@ def clean(soup_obj):
 
 def write(data):
     '''append jsonified data to file'''
-    data_json = json.dumps(data)
+    data_json = json.dumps(data, sort_keys=True)
 
     with open('weather.json', 'a') as f:
         f.write(data_json + '\n')
@@ -113,17 +102,20 @@ def to_series(data_list):
         'wind_spd': list(),
         'wind_gust': list(),
         'wind_dir': list(),
-        # 'weather': weather
     }
 
+    # do conversion by processing each dict "data" with a loop over
+    # the keys in "series". This ensures that none of the lists in "series"
+    # are missing a data point if a particular parameter was not in
+    # the original data.
     for data in data_list:
         for k in series.keys():
-            v = data.get(k) # returns the item or None
+            v = data.get(k)  # returns the item or None
             if v is None:
-                item = v # customize what to do with non-existent data
+                item = v  # customize what to do with non-existent data
             elif k == 'retrieved':
-                item = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S').replace(
-                    second=0)
+                item = datetime.strptime(
+                    v, '%Y-%m-%dT%H:%M:%S').replace(second=0)
             else:
                 item = float(v)
 
@@ -167,31 +159,55 @@ def plot(series):
         angle=series['wind_dir'],
         angle_units='deg',
         color='lightgreen',
-        size=12)
+        size=10)
     p.rect(
         series['retrieved'],
         series['wind_spd'],
         angle=series['wind_dir'],
         angle_units='deg',
         color='lightgreen',
-        width=1,
-        height=25,
+        width=0.5,
+        height=20,
         height_units='screen')
-    p.circle(
+    p.rect(  # looks better than circle for wind gusts
         series['retrieved'],
         series['wind_spd'],
-        radius=series['wind_gust'],
-        radius_units='screen',
+        # angle=series['wind_dir'],
+        # angle_units='deg',
         color='green',
-        fill_color=None,
-        legend='wind gust')
-    # p.line(series['retrieved'], series['wind_dir'], line_dash='4 4', color='green', legend='wind dir deg')
+        width=1,
+        height=series['wind_gust'],
+        height_units='screen')
+
+    # precipitation related graphs
+    shifted_time = series['retrieved'][1:] + [
+        series['retrieved'][-1] + timedelta(hours=1)
+    ]
+    p.quad(
+        legend='Rain (in)',
+        left=series['retrieved'],
+        right=shifted_time,
+        top=series['rain'],
+        bottom=0,
+        color='blue',
+        alpha=0.2)
+    p.quad(
+        legend='Snow (in)',
+        left=series['retrieved'],
+        right=shifted_time,
+        top=series['snow'],
+        bottom=0,
+        color='red',
+        alpha=0.2)
+
+    # legend setting has to be here to actually take effect
+    p.legend.location = 'top_left'
 
     output_file('weather_plot.html')
     show(p)
 
 
-if __name__ == '__main__':
+def main():
     if len(sys.argv) > 1 and sys.argv[1] == 'plot':
         plot(to_series(read()))
     else:
@@ -199,3 +215,6 @@ if __name__ == '__main__':
         data = clean(soup_obj)
         write(data)
 
+
+if __name__ == '__main__':
+    main()
