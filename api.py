03655695fdedc431d0b01f52e52b3670388a287d
Ben <ben@laptop.quillaja.net>
2017-12-14 04:54:43 -0800

    first draft of returning bokeh plots in the api

--- api.py
+++ api.py
@@ -1,12 +1,16 @@
-import json
 import datetime as dt
+import json
 import time
 from distutils.util import strtobool
-from flask import Flask, request, abort
+
+from bokeh.plotting import figure, output_file, show
+from bokeh.embed import components, file_html
+from flask import Flask, abort, request
 from peewee import DoesNotExist
+
 import queries
-from model import Mountain, Cam, ScrapeRecord
 import settings
+from model import Cam, Mountain, ScrapeRecord
 
 app = Flask(__name__)
 
@@ -18,12 +22,13 @@ def to_sys_time(t: dt.datetime, mttz: dict) -> dt.datetime:
         return t
 
     return (t - dt.timedelta(seconds=(mttz['rawOffset'] + mttz['dstOffset']))
-            ) - (dt.timedelta(seconds=time.timezone) - dt.timedelta(
-                hours=time.localtime().tm_isdst))
+            ) - (dt.timedelta(seconds=time.timezone) -
+                 dt.timedelta(hours=time.localtime().tm_isdst))
 
 
-def convert_input(time_input: str, as_local_time: bool=False,
-                  mttz: dict=None) -> dt.datetime:
+def convert_input(time_input: str,
+                  as_local_time: bool = False,
+                  mttz: dict = None) -> dt.datetime:
     '''
     Parses a time string. If `as_local_time` is True, it will use the
     timezone info provided in `mttz` and the system's timezone info to
@@ -160,6 +165,112 @@ def weather_to_json(forecasts):
     return json.dumps(data, indent=2, sort_keys=True)
 
 
+def weather_to_bokeh(forecasts, mt_name):
+    '''makes nice plot from the forecast data'''
+    p = figure(
+        title=mt_name + ' Summit - Forecast Weather Each Hour',
+        x_axis_label='time',
+        x_axis_type='datetime',
+        y_axis_label='unit',
+        width=1000)
+
+    created = [w.created for w in forecasts]
+
+    # temperature related lines
+    p.line(
+        created, [w.temp for w in forecasts],
+        line_width=2,
+        color='black',
+        legend='Temp (F)')
+    p.line(
+        created, [w.temp_max for w in forecasts],
+        legend='Max Temp',
+        line_dash='4 4',
+        color='red',
+        line_width=0.5)
+    p.line(
+        created, [w.temp_min for w in forecasts],
+        legend='Min Temp',
+        line_dash='4 4',
+        color='blue',
+        line_width=0.5)
+
+    # wind related lines
+    # for some stupid reason bokeh draws angles counter clockwise, so
+    # i have to negate all the wind_dir entries to make it display right.
+    #
+    wind_dir_inverted = [
+        -(w.wind_dir if w.wind_dir is not None else 0) for w in forecasts
+    ]
+    wind_spd = [w.wind_spd for w in forecasts]
+    p.line(
+        created,
+        wind_spd,
+        line_width=2,
+        color='lightgreen',
+        legend='Wind (mph)')
+    p.vbar(  # looks better than circle for wind gusts
+        x=created,
+        bottom=wind_spd,
+        color='lightgreen',
+        width=0.5,
+        top=[w.wind_gust
+             for w in forecasts])  # not pretty for missing values (drawn as 0)
+    p.inverted_triangle(  # use inverted_triangle to make display 'map view'
+        created,
+        wind_spd,
+        angle=wind_dir_inverted,
+        angle_units='deg',
+        color='green',
+        size=8)
+    p.rect(
+        created,
+        wind_spd,
+        angle=wind_dir_inverted,
+        angle_units='deg',
+        color='green',
+        width=0.5,
+        height=16,
+        height_units='screen')
+
+    # precipitation related graphs
+    shifted_time = created[1:] + [created[-1] + dt.timedelta(hours=1)
+                                  ] if len(forecasts) > 0 else []
+    p.quad(
+        legend='Rain (in)',
+        left=created,
+        right=shifted_time,
+        top=[w.rain for w in forecasts],
+        bottom=0,
+        color='blue',
+        alpha=0.2)
+    p.quad(
+        legend='Snow (in)',
+        left=created,
+        right=shifted_time,
+        top=[w.snow for w in forecasts],
+        bottom=0,
+        color='red',
+        alpha=0.2)
+
+    # legend setting has to be here to actually take effect
+    p.legend.location = 'top_left'
+    p.legend.orientation = 'horizontal'
+    p.legend.background_fill_alpha = 0.5
+    p.legend.click_policy = 'hide'
+
+    # grid settings
+    p.xaxis.minor_tick_line_color = 'black'
+    p.xaxis.minor_tick_line_width = 1
+    p.xgrid.minor_grid_line_color = 'gray'
+    p.xgrid.minor_grid_line_alpha = 0.2
+    p.ygrid.minor_grid_line_color = 'gray'
+    p.ygrid.minor_grid_line_alpha = 0.2
+
+    from bokeh.resources import CDN
+    return file_html(p, CDN, mt_name)
+
+
 @app.route('/api/mountains/<int:mt_id>/weather')
 def do_weather(mt_id):
     '''Returns weather data for the mountain during the dates specified by
@@ -188,7 +299,14 @@ def do_weather(mt_id):
     end = convert_input(end, as_local_time, mttz)
     start = convert_input(start, as_local_time, mttz)
 
-    forecasts = queries.weatherforecasts_for_mt(mt.id)
+    forecasts = queries.weatherforecasts_for_mt(mt.id, start, end)
 
-    # TODO: add bokeh format stuff
-    return weather_to_json(forecasts)
+    # bokeh is the default if nothing is specified in the GET format
+    # param, but if invalid junk is specified, send json by default
+    # because it's less expensive to process.
+    if frmt == 'bokeh':
+        # script, div = weather_to_bokeh(forecasts, mt.name)
+        # return div + script  # seems to work
+        return weather_to_bokeh(forecasts, mt.name)
+    else:
+        return weather_to_json(forecasts)
